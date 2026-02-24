7501d35ce775ad7d4951d504f641cb3cb8afe317
HathalerMichael <m.hathaler@students.htl-leonding.ac.at>
2026-02-24 22:00:55 +0100

    added buisness logic

--- HtlMaturaStarter/WebApi/Logic/ImpactScoreCalculator.cs
+++ HtlMaturaStarter/WebApi/Logic/ImpactScoreCalculator.cs
@@ -2,6 +2,34 @@ public static class ImpactScoreCalculator
 {
     public static double Calculate(PlayerDto player, MatchDetailsDto match)
     {
-        throw new NotImplementedException();
-    }
+        if (player.PlayedMinutes < 0)
+        {
+            return -1.00;
+        }
+        
+        double impactScore = 6;
+
+        EventDto[] events = match.Events.Where(e => e.Name == player.Name).ToArray();
+
+        if (events.Count(e => e.Action == "Goal" || e.Action == "Assist") == 3)
+        {
+            impactScore = 10;
+        }
+        else
+        {
+            foreach(EventDto eventDto in events)
+        {
+            switch (eventDto.Action)
+            {
+                case "Goal": impactScore += 2; break;
+                case "Assist": impactScore += 1; break;
+                case "YellowCard": impactScore -= 1;break;
+                case "RedCard": impactScore -= 3; break;
+            }
+        }
+        }
+        
+        return impactScore;
+    } 
 }
+
